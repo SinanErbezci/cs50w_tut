5b1b9a1fdbb75d485974423c07daf95792a32fa9
Sinan Erbezci <sinanerbezci@Sinans-MacBook-Air.local>
2024-07-17 16:21:20 +0300

    new changes

--- week3/airline/flights/admin.py
+++ week3/airline/flights/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Flight, Airport, Passenger
+
+# You can use some settings from django for gui
+
 # Register your models here.
+admin.site.register(Airport)
+admin.site.register(Flight)
+admin.site.register(Passenger)
--- week3/airline/flights/models.py
+++ week3/airline/flights/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 # Create your models here.
 # Think as a table in db
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    # If you delete an airport from Airport, flight including that airport would be also deleted.
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departure")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
 # Migrations. Means the changes you made.
 # Then you migrate them which means applying into the database
 # python manage.py makemigrations
