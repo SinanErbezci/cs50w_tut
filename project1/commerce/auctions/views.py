515d09a8df29ce855cc3dcb65ef74fcf272f4b31
Sinan Erbezci <sinanerbezci@Sinans-MacBook-Air.local>
2024-07-19 20:03:57 +0300

    new stuff

--- project1/commerce/auctions/views.py
+++ project1/commerce/auctions/views.py
@@ -3,8 +3,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import User
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from .models import User, Auctions
 
 
 def index(request):
@@ -63,6 +64,40 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def create_listing(request):
+    
+    category_dict = Auctions.CATEGORY_CHOICES
+    message = ""
     if request.method == "POST":
-        pass
-    return render(request, "auctions/create_listing.html")
+        desc = request.POST.get("description",False)
+        category = request.POST.get("category", False)
+        url = request.POST.get("url",False)
+        price1 = request.POST.get("price-1",False)
+        price2 = request.POST.get("price-2")
+        print(category)
+        if not desc:
+            message = "Please Enter description"
+        elif not category:
+            message = "Please Select category"
+        elif not price1 or not price1.isdigit():
+            message = "Please Enter Price"
+        elif url:
+            validate = URLValidator()
+            try:
+                validate(url)
+            except ValidationError as e:
+                message = e.message
+        
+        price = int(price1) + (int(price2)/100.0)
+        new_item = Auctions.objects.create(
+            description=desc, category=category, image=url, price=price
+            )
+        new_item.save()
+        print(price)
+        return render(request, "auctions/create_listing.html", {
+            "category_dict": category_dict,
+            "message": message
+            })
+
+    return render(request, "auctions/create_listing.html", {
+        "category_dict": category_dict
+    })
