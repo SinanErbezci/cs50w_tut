5e751f2a6976058300010ca8f784fa1d50d34bc5
Sinan Erbezci <sinanerbezci@172-2-0-148.lightspeed.dybhfl.sbcglobal.net>
2024-09-06 14:36:44 +0300

    reverted back

--- project4/network/static/network/posts.js
+++ project4/network/static/network/posts.js
@@ -5,17 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('#follow-button').addEventListener('click', follow);
     }
 
-    let likeBtns = document.querySelectorAll('#like-btn');
+    let likeBtns = document.querySelectorAll('.like-btn');
     if (typeof likeBtns != "undefined") {
     likeBtns.forEach((button) => {
         button.onclick = like;
     });}
-
-    let editBtns = document.querySelectorAll('#edit-btn');
-    if (typeof editBtns != "undefined") {
-        editBtns.forEach((button) => {
-            button.onclick = edit;
-        });}
 
   });
 
@@ -71,11 +65,4 @@ function like() {
             parent.lastChild.innerHTML = parseInt(parent.lastChild.innerHTML) + -1;
         }
     })
-}
-
-function edit() {
-    let postDiv = this.parentElement;
-    let content = postDiv.children[2].innerHTML;
-    
-    
 }
