81c71418bfbb850b1966dcd5d76366ee6d0294be
Sinan Erbezci <sinanerbezci@172-2-0-148.lightspeed.dybhfl.sbcglobal.net>
2024-09-08 15:45:36 +0300

    last changes

--- project2/mail/mail/static/mail/inbox.js
+++ project2/mail/mail/static/mail/inbox.js
@@ -25,9 +25,18 @@ function compose_email(prefill=false) {
 
   // Clear out composition fields
   if (prefill == true) {
-    document.querySelector('#compose-recipients').value = document.querySelector('#to').innerHTML;
-    document.querySelector('#compose-subject').value = document.querySelector('#subject').innerHTML;
-    document.querySelector('#compose-body').value = document.querySelector('.mail-body__p').innerHTML;
+    sender = document.querySelector('#from').innerHTML;
+    exSubject = document.querySelector('#subject').innerHTML;
+    if (exSubject.slice(0,4) === "Re: ") {
+      preSubject = "";
+    }
+    else {
+      preSubject = "Re: ";
+    }
+    preBody = `On ${document.querySelector('#time').innerHTML} ${sender} wrote:`;
+    document.querySelector('#compose-recipients').value = sender;
+    document.querySelector('#compose-subject').value = preSubject.concat(document.querySelector('#subject').innerHTML);
+    document.querySelector('#compose-body').value = preBody.concat(document.querySelector('.mail-body__p').innerHTML);
   }
 }
 
@@ -45,46 +54,42 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(email => {
-    // Print email
-    console.log(email);
-
-    if (email.length > 0) {
       for (let i = 0; i < email.length; i++) {
         let row = document.createElement('div');
         let sender = document.createElement('p');
         let topic = document.createElement('p');
         let time = document.createElement('p');
 
-        if (mailbox === 'inbox') {
-          sender.innerHTML = email[i].sender;
-        }
-        else if (mailbox === 'sent') {
-          if (email[i].recipients.length > 1) {
-            sender.innerHTML = email[i].recipients[0].concat(' + more')
-          }
-          else {
-            sender.innerHTML = email[i].recipients[0]
-          }
-        }
-        // sender.innerHTML = email[i].sender;
-        topic.innerHTML = email[i].subject;
-        time.innerHTML = email[i].timestamp;
-
-        row.classList.add('emails-container');
-        if ((email[i].read === true) && (mailbox === 'inbox')) {
-          row.classList.add('read');
-        }
-        row.addEventListener('click', () => view_mail(email[i].id));  
-        sender.classList = 'sender';
-        topic.classList = 'topic';
-        time.classList = 'time';
-  
-        emailDiv.appendChild(row);
-        row.appendChild(sender);
-        row.appendChild(topic);
-        row.appendChild(time);
+      if (mailbox === 'inbox') {
+        sender.innerHTML = email[i].sender;
       }
+      else if (mailbox === 'sent') {
+        if (email[i].recipients.length > 1) {
+          sender.innerHTML = email[i].recipients[0].concat(' + more')
+        }
+        else {
+          sender.innerHTML = email[i].recipients[0]
+        }
+      }
+      // sender.innerHTML = email[i].sender;
+      topic.innerHTML = email[i].subject;
+      time.innerHTML = email[i].timestamp;
+
+      row.classList.add('emails-container');
+      if ((email[i].read === true) && (mailbox === 'inbox')) {
+        row.classList.add('read');
+      }
+      row.addEventListener('click', () => view_mail(email[i].id));  
+      sender.classList = 'sender';
+      topic.classList = 'topic';
+      time.classList = 'time';
+
+      emailDiv.appendChild(row);
+      row.appendChild(sender);
+      row.appendChild(topic);
+      row.appendChild(time);
     }
+
 
 
 
@@ -99,25 +104,27 @@ function send_mail(event) {
   const subject = document.querySelector('#compose-subject').value;
   const receivers = document.querySelector('#compose-recipients').value;
 
-    if ( body === '' || subject === '' || receivers === ''){
-      alert('Please fill all of the parts');
-      }
-    else {
-      fetch('/emails', {
-        method: 'POST',
-        body: JSON.stringify({
-            recipients: receivers,
-            subject: subject,
-            body: body
-        })
-      })
-      .then(response => response.json())
-      .then(result => {
-          // Print result
-          console.log(result);
-      });
-    load_mailbox('sent')
+  if ( body === '' || subject === '' || receivers === ''){
+    alert('Please fill all of the parts');
     }
+  else {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: receivers,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+        load_mailbox('sent')
+
+    });
+  // load_mailbox('sent')
+  }
 
   return false;
   }
@@ -129,18 +136,24 @@ function view_mail(mail_id) {
   document.querySelector('#view-mail').style.display = 'block';
   document.querySelector('#archieve').style.display = 'inline';
 
-  document.querySelector('#archieve').dataset.id = mail_id;
+  const archieveBtn = document.querySelector('#archieve');
+  archieveBtn.dataset.id = mail_id;
 
   fetch(`/emails/${mail_id}`)
   .then(response => response.json())
   .then(email => {
-
-
     document.querySelector('#from').innerHTML = email.sender;
     document.querySelector('#to').innerHTML = email.recipients.toString();
     document.querySelector('#subject').innerHTML = email.subject;
     document.querySelector('#time').innerHTML = email.timestamp;
     document.querySelector('.mail-body__p').innerHTML = email.body;
+
+    if (email.archived === true) {
+      archieveBtn.innerHTML = "Unarchieve";
+    }
+    else {
+      archieveBtn.innerHTML = "Archieve";
+    }
 
   });
 
@@ -154,11 +167,20 @@ function view_mail(mail_id) {
 
 function archieve() {
 
+  if (this.innerHTML === "Archieve") {
   fetch(`/emails/${this.dataset.id}`, {
     method: 'PUT',
     body: JSON.stringify({
         archived: true
     })
-  });
-  load_mailbox('inbox');
+  })
+  .then(() => load_mailbox('inbox'));}
+  else {
+    fetch(`/emails/${this.dataset.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: false
+      })
+    })
+    .then(() => load_mailbox('inbox'));}
 }
